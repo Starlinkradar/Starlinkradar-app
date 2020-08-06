53761ae2f6dd764fd71ff9b5fe390512398f8003
Larko <59736843+Larko00@users.noreply.github.com>
2020-08-05 23:49:03 -0400

    added website & discord button

--- lib/tabs/about.dart
+++ lib/tabs/about.dart
@@ -27,17 +27,25 @@ class _Aboutme extends State<About> {
         children: <Widget>[
           Icon(
             Icons.satellite,
-            size: 100,
+            size: 75,
           ),
           Text(
             "\nHey! I'm Aymen, the developer of Starlinkradar!\n\nEverything I have created so far is 100% out of my own pocket so please consider supporting me to keep Starlinkradar up and free of advertisements!\n",
-            style: TextStyle(fontSize: 25),
+            style: TextStyle(fontSize: 20),
             textAlign: TextAlign.center,
           ),
           OutlineButton(
             onPressed: () => _launchURL("https://ko-fi.com/aymen"),
             child: Text("Support me!"),
-          )
+          ),
+          OutlineButton(
+            onPressed: () => _launchURL("https://discord.gg/BHdGqAX"),
+            child: Text("Discord"),
+          ),
+          OutlineButton(
+            onPressed: () => _launchURL("https://starlinkradar.com/"),
+            child: Text("Website"),
+          ),
         ],
       ),
       margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
